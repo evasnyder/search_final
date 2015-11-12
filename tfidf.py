23054a14cf079559e263b097c2e89c1d8b3b514c
Eva Snyder <snyde24e@mtholyoke.edu>
2015-11-12 16:55:45 -0500

    best

--- tfidf.py
+++ tfidf.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import re, string, math, random
+from porter2 import stem
 
 
 punct = re.compile(r'[\s{}]+'.format(re.escape(string.punctuation)))
@@ -15,15 +16,23 @@ def tokenize(file_name):
 	sentences_d.remove('')
 	return sentences_d
 
+def remove_stopwords(): 
+	stopwords_file = open('stopwords.txt', 'r')
+	stopwords = stopwords_file.read()
+	stopwords = stopwords.split('\n')
+	stopwords_file.close()
+	return stopwords
+
 # create a list of query term frequencies per word
-def create_querytf_list(query_list):
+def create_querytf_list(query_list, stopwords):
 	querytf_list = list()
 	for query in query_list:
 		# lowercase every query word
 		formatted_query = punct.split(query.lower())
-		# save the number as the first thing
+		formatted_query = [w for w in formatted_query if w not in stopwords]
+		# formatted_query = [stem(w) for w in formatted_query]
+		# truncate number from query
 		formatted_query = formatted_query[1:]
-		# create a dictionary for the term frequencies of the query
 		querytf_dict = dict()
 		# create a list of unique query words
 		unique_words = set(formatted_query)
@@ -33,19 +42,23 @@ def create_querytf_list(query_list):
 		#generate tf counts for each unique word in query
 		for unique_word in unique_words:
 			# if the query we're looking out has the unique word, calculate the number of times it appears
-			querytf_dict[unique_word] = formatted_query.count(unique_word)#/len(formatted_query)
+			querytf_dict[unique_word] = formatted_query.count(unique_word)
 		querytf_list.append(querytf_dict)
 	return querytf_list
 
-# Create the list of document words formatted and split correctly
-def create_docword_list(doc_list):
+# Creat the list of document words formatted and split correctly
+def create_docword_list(doc_list, stopwords):
 	docword_list = list()
 	# for every document in our collection
 	for doc in doc_list:
 		# lower case everything
 		formatted_doc = punct.split(doc.lower())
-		# save the first value as the number of the document
+		formatted_doc = [w for w in formatted_doc if w not in stopwords]
+		# formatted_doc = [stem(w) for w in formatted_doc]
+
+		# save the number as the first thing
 		formatted_doc = formatted_doc[1:]
+
 		# remove any extra whitespace
 		if '' in formatted_doc:
 			formatted_doc.remove('')
@@ -75,7 +88,7 @@ def get_avg_doclength(documents, collection_length):
 	return (sum/collection_length)
 
 def output_similarities(documents, querytf_list, documenttf, collection_length, avg_doclength):
-	f = open('tfidf.top', 'w')
+	f = open('best.top', 'w')
 	# for every query in the list of query term frequencies
 	for query_index, query in enumerate(querytf_list):
 		# for every document 
@@ -94,12 +107,12 @@ def output_similarities(documents, querytf_list, documenttf, collection_length, 
 	f.close()
 
 
-
 my_queries = tokenize('qrys.txt')
 my_documents = tokenize('docs.txt')
-my_docs = create_docword_list(my_documents)
+my_stopwords = remove_stopwords()
+my_docs = create_docword_list(my_documents, my_stopwords)
 my_documenttf = create_documenttf_dict(my_docs)
-my_querytf = create_querytf_list(my_queries)
+my_querytf = create_querytf_list(my_queries, my_stopwords)
 my_collection_length = len(my_docs)
 my_avg_doclength = get_avg_doclength(my_docs, my_collection_length)
 
