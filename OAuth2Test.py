28c490d23796cd4d96f9c14081592ec5026af967
Eva Snyder <snyde24e@mtholyoke.edu>
2015-11-12 16:37:57 -0500

    OAuth

---
+++ OAuth2Test.py
@@ -0,0 +1,9 @@
+from rauth import OAuth2Service
+
+genius = OAuth2Service(
+    client_id='azPARsJ8P5HyP_msTQmSQq1pB7EjHmQLP6EuSnVYnrd7ACivST7j8eOKpw53BBkc',
+    client_secret='XZ8uklwLdkzk1bsrj1FICspuQKPl8xipRZD7kP6EunL9FN7ZxOb_EGW4xRrhD3cdi5k3RP0tt-Zyi_7F6ttLiQ',
+    name='genius',
+    authorize_url='https://api.genius.com/oauth/authorize',
+    access_token_url='https://api.genius.com/oauth/50Li8ZJ-fN7eCvpeFQTVfkS1ttoWnJMZKkXIxxqax9oBRCoNJ9xJvksqKEHNILCy',
+    base_url='https://genius.com/')
