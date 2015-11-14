ae57ebce3fe1dcc0dad95d8faf72c3727729a483
Eva Snyder <snyde24e@mtholyoke.edu>
2015-11-14 15:39:09 -0500

    shit?

--- testdatabase.py
+++ testdatabase.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+
 import MySQLdb
 
 # Open database connection
@@ -7,13 +8,18 @@ db = MySQLdb.connect("localhost","testuser","test123","TESTDB" )
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
-# execute SQL query using execute() method.
-cursor.execute("SELECT VERSION()")
+# Drop table if it already exist using execute() method.
+cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
 
-# Fetch a single row using fetchone() method.
-data = cursor.fetchone()
+# Create table as per requirement
+sql = """CREATE TABLE EMPLOYEE (
+         FIRST_NAME  CHAR(20) NOT NULL,
+         LAST_NAME  CHAR(20),
+         AGE INT,  
+         SEX CHAR(1),
+         INCOME FLOAT )"""
 
-print "Database version : %s " % data
+cursor.execute(sql)
 
 # disconnect from server
 db.close()
