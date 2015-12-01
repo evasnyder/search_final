c8506208b2adeed49453d0157d6cec3bd05c75b2
Eva Snyder <snyde24e@mtholyoke.edu>
2015-11-30 20:32:17 -0500

    Fixed Variable Names

--- sampleDetection.py
+++ sampleDetection.py
@@ -1,12 +1,12 @@
-import positIndex, string, re
+import posititionalIndex, string, re
 
 
 punct = re.compile(r'[\s{}]+'.format(re.escape(string.punctuation)))
 
-lyrics = positIndex.tokenizeFile('test_songs.txt')
-my_stopwords = positIndex.get_stopwords()
-my_docs = positIndex.create_docword_list(lyrics, my_stopwords)
-positIndex = positIndex.create_posit(my_docs)
+lyrics = posititionalIndex.tokenizeFile('test_songs.txt')
+my_stopwords = posititionalIndex.get_stopwords()
+my_docs = posititionalIndex.create_docword_list(lyrics, my_stopwords)
+posititional_Index = posititionalIndex.create_posit(my_docs)
 
 query = 'Straight up now'
 
@@ -18,32 +18,32 @@ query_word_list = {}
 positional_list = {}
 
 
-def getLists(query, positIndex):
+def getLists(query, posititional_Index):
 	smallest_value = 9999999999;
 	wordInLeastDocuments = " ";
 
 	docsToCheck = []
 
 	for word in query:
-		print positIndex[word]
-		currentNumKeys = len(positIndex[word].keys())
-		print "How Many Documents: " + str(len(positIndex[word].keys()))
+		print posititional_Index[word]
+		currentNumKeys = len(posititional_Index[word].keys())
+		print "How Many Documents: " + str(len(posititional_Index[word].keys()))
 
 		if currentNumKeys < smallest_value:
 			smallest_value = currentNumKeys
 			wordInLeastDocuments = word
 
    	print "Word in the least amount of docs: " + str(wordInLeastDocuments) + ' ' +  str(smallest_value)
-   	print positIndex[wordInLeastDocuments].keys()
+   	print posititional_Index[wordInLeastDocuments].keys()
 
-   	for doc in positIndex[wordInLeastDocuments].keys():
+   	for doc in posititional_Index[wordInLeastDocuments].keys():
    		for word in query: 
-   			if doc not in positIndex[word].keys():
+   			if doc not in posititional_Index[word].keys():
    				break
    			if doc not in docsToCheck:
    				docsToCheck.append(doc)
    	print 'docs to check: ' + str(docsToCheck)
 
-getLists(formatted_query, positIndex)
+getLists(formatted_query, posititional_Index)
 
 
