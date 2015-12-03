df5e97d7917d17a6d4bcb09542e120ab3d1466bb
Adam Mullarkey <acmullarkey@gmail.com>
2015-12-02 19:30:36 -0500

    Removed lyrics file for the moment

--- recusionForPlagiarism.py
+++ recusionForPlagiarism.py
@@ -1,6 +1,6 @@
 positional_lists_for_document = {}
 
-def match_check(previous_index, query, document_id):
+def detect_sample(previous_index, query, document_id):
 	global positional_lists_for_document
 	current_positional_list = positional_lists_for_document[query[0]][document_id]
 	speculative_index = 
@@ -10,7 +10,7 @@ def match_check(previous_index, query, document_id):
 		if len(query) == 1:
 			return True
 		else:
-			return match_check(next_index, query[1:])
+			return detect_sample(next_index, query[1:])
 	else:
 		return False
 
@@ -21,8 +21,8 @@ def compareLists(query, relevant_positional_index, possible_document_matches):
 	# Tokenize query
 	for document in possible_document_matches:
 			for position in positional_index[query[0]][document['_id']]:
-				verified_flag = match_check(position, query, document['_id'])
-				if verified_flag == True:
+				song_contains_query = detect_sample(position, query, document['_id'])
+				if song_contains_query:
 					sampled_songs.append(document)
 					break
 
