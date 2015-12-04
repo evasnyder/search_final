b4f9973f800e31cd4aa0b857dd3361ec02d3030a
Eva Snyder <snyde24e@mtholyoke.edu>
2015-12-04 16:45:50 -0500

    workin'

--- recusionForPlagiarism.py
+++ recusionForPlagiarism.py
@@ -1,7 +1,7 @@
 positional_lists_for_document ={
-	"pizzas" : { "doc1" : [1,2,3,4,5,6,7,8], "doc2": [3,6,8,9,10,15], "doc3": [4,5,8,12,66]},
-	"are": {"doc1" : [9,27,88,100], "doc2": [4,5,11,18]},
-	"dope": {"doc1" : [10], "doc3": [6, 11, 13, 68]}
+	"pizzas" : { "doc1" : [1,2,3,4,5,6,7,8], "doc2": [3,6,8,9,10,15], "doc3": [4,5,8,12,66], "doc4": [1,2,3,4,10]},
+	"are": {"doc1" : [9,27,88,100], "doc2": [4,5,11,18], "doc4": [5, 11]},
+	"dope": {"doc1" : [10], "doc3": [6, 11, 13, 68], "doc4": [6]}
 }
 
 
@@ -13,10 +13,8 @@ def detect_sample(current_index, query, document_id):
 	# [20, 40, 67]
 	current_positional_list = positional_index[query[0]][document_id]
 	print "Current Positional List: " + str(current_positional_list)
-	if len(query) != 1:
-		next_index = current_positional_list.index(current_index+1) if current_index+1 in current_positional_list else None
-	else:
-		return True
+
+	next_index = current_positional_list.index(current_index+1) if current_index+1 in current_positional_list else None
 
 	print "Next Index: " + str(next_index)
 	print len(query)
@@ -28,7 +26,7 @@ def detect_sample(current_index, query, document_id):
 		else:
 			print "We're inside the else!!!"
 			print query[1:]
-			return detect_sample(next_index, query[1:], document_id)
+			return detect_sample(current_index+1, query[1:], document_id)
 	else:
 		return False
 
@@ -59,4 +57,4 @@ def compareLists(query, relevant_positional_index, possible_document_matches):
 
 	return sampled_songs
 
-print compareLists(["pizzas", "are", "dope"], positional_lists_for_document, ["doc1"])
+print compareLists(["pizzas", "are", "dope"], positional_lists_for_document, ["doc1", "doc4"])
