7cd0ce52fd7f58802b8c0bbc593fb24672018740
Eva Snyder <snyde24e@mtholyoke.edu>
2015-12-12 17:09:32 -0500

    prints tfidf to command line

--- tfidf.py
+++ tfidf.py
@@ -105,6 +105,7 @@ def calculateTfidf(query, positional_index, list_of_matching_documents, avg_song
 			raw_tf = len(positional_index[word]['document_dict'][song])
 			songtf = len(positional_index[word]['document_dict'])
 			similarity += (querytf * (raw_tf)/(raw_tf + (k * len(song)/avg_songlength))) * math.log10(collection_length/songtf)
+		print (str(song) + ' 0 ' + str(similarity) + ' 0\n')
 		f.write(str(song) + ' 0 ' + str(similarity) + ' 0\n')
 	f.close()
 
